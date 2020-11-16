285c6f0c14f29974ffcd71df24437212bf2ff096
alijosephine <alijosephine@gmail.com>
2020-11-16 13:01:33 +0530

    Attempting Q4

--- sheet01_7010048_7010829_7012336.R
+++ sheet01_7010048_7010829_7012336.R
@@ -184,21 +184,24 @@ lines(density(stories$obs), col = "blue")
 ##    (will become the x-axis in the plot).
 ##    Get R to generate the range from -5 to 5, by 0.1. Assign this to the 
 ##    variable x.
+x <- seq(-5, 5, by=0.1)
 
 
 ## b) Now we need to obtain the y-values of the plot (the density). We do this 
 ##    using the density function for the normal distribution. 
 ##    Use "help(dnorm)" to find out about the standard functions for the normal 
 ##    distribution.
-
+help(dnorm)
+y <- dnorm(x,mean=0,sd=1,log=FALSE)
 
 ## c) Now use plot() to plot the normal distribution for z values of "x". 
-
+plot(x,y)
 
 ## d) The plot now has a relatively short y-range, and it contains circles 
 ##    instead of a line. 
 ##    Using plot(), specify the y axis to range from 0 to 0.8, and plot a line 
 ##    instead of the circles.
+plot(x,y,type="l",col="blue",ylim=c(0,0.8))
 
 
 ## e) We want to have a vertical line to represent the mean of our distribution.
@@ -206,17 +209,39 @@ lines(density(stories$obs), col = "blue")
 ##    Use abline() to create the vertical line. Specify the median of x using
 ##    the argument 'v'.
 ##    In order to get a dashed line, set the argument 'lty' to 2.
+abline(v=0,lty=2)
+## adds lines to already existing plot
+
 
 ## f) Take a look at the beaver1 dataset. (You can see it by typing "beaver1".) 
 ##    Then select only the temperature part and store it in a variable "b1temp".
+beaver1
+b1temp <- beaver1$temp
 
 ## g) Calculate the mean and standard deviation of this dataset and plot a normal
 ##    distribution with these parameters.
+(b1mean <- mean(b1temp))
+(b1sd <- sd(b1temp))
+plot(b1temp,dnorm(b1temp, mean=b1mean, sd=b1sd, log = FALSE))
+## DOUBT - what does this plot represent? densities of observed values as if it were a normal distribution and therefore, not the actual densities?? 
 
 ## h) We observe two temparatures (36.91 and 38.13). What's the likelihood that
 ##    these temperatures (or more extreme ones) respectively come 
 ##    from the normal distribution from g)?
+pnorm(36.91,mean=b1mean, sd=b1sd, log = FALSE)
+## the cumulative probability (i.e, 36.91 or lower) is 0.5976 --> the probability of 36.91 or more extreme values is 1 - 0.5976 = 0.4024
+## likely that this comes from a normal distribution
+pnorm(38.13,mean=b1mean, sd=b1sd, log = FALSE)
+## the cumulative probability (i.e, 38.13 or lower) is 1 --> the probability of 36.91 or more extreme values is 1 - 1 = 0 
+## very unlikely that this comes from a normal distribution
+
 
 ## i) Use the random sampling function in R to generate 20 random samples from
 ##    the normal distribution from g), and draw a histogram based on this sample.
 ##    Repeat 5 times. What do you observe?
+## DOUBT - 20 samples or 5 samples of size 20?? (assuming the latter and proceeding)
+b1sample <- rnorm(20,mean=b1mean, sd=b1sd)
+hist(b1sample,breaks=10)
+## the distributions vary widely between the different random samples
+
+
