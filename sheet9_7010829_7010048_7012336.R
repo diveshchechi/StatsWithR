189ed215358c8d55768aaafbf0768e74c6cc7335
diparani1995 <74528455+diparani1995@users.noreply.github.com>
2021-01-25 20:48:27 +0100

    Update sheet9_7010829_7010048_7012336.R

--- sheet9_7010829_7010048_7012336.R
+++ sheet9_7010829_7010048_7012336.R
@@ -2,6 +2,9 @@
 #Week 11: Model Families and Logistic Regression
 ##########################
 library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(lme4)
 
 ## This exercise sheet contains the exercises that you will need to complete and 
 ## submit by 23:55 on Monday. Write the code below the questions. 
@@ -35,7 +38,7 @@ library(dplyr)
 # (Fisman et al., 2006). For each date, each person recorded several subjective
 # numerical ratings of the other person (attractiveness, compatibility, and some 
 # other characteristics) and also wrote down whether he or she would like to meet
-# the other person again. Label and rij1, . . . , rij6 as person i’s numerical ratings of person j 
+# the other person again. Label and rij1, . . . , rij6 as person iâ€™s numerical ratings of person j 
 # on the dimensions of attractiveness, compatibility, and so forth.
 speeddating <- read.csv(file="Speed Dating Data.csv")
 summary(speeddating)
@@ -47,24 +50,42 @@ head(speeddating)
 #    abbreviations stand for.
 #    Also, please plot the data in order to inspect it, and discuss the importance of attractiveness, compatibility, and so 
 #    forth in this predictive model.
+mylogit <- glm(data = speeddating, dec ~ attr + sinc + intel + fun, family = "binomial")
+
+ggplot(gather(subset(speeddating,select=-c(dec)), cols, value), aes(x = value)) + geom_histogram()+facet_wrap(~cols)
+
+ggplot(gather(subset(speeddating,select=-c(dec)), cols, value), aes(x = value)) + geom_boxplot()+facet_wrap(~cols)
+#predictors intel and sinc has more outliers compared to attr and fun
+#this also verifies the positive skewness of the predictors intel and sinc
+
+summary(mylogit)
+#p value of intel is too high ie 0.22 which shows that it is insignificant
+#p value is of attr and fun is less hence they are most significant.
+
+plot(mylogit)
 
 
 #(2) Expand this model to allow varying intercepts for the persons making the
 #    evaluation; that is, some people are more likely than others to want to meet
 #    someone again. Discuss the fitted model. 
+randInt_iid <- glmer(dec ~ attr + sinc + intel + fun +(1 | iid), data = speeddating, family = binomial)
+summary(randInt_iid)
 
 #(3) Expand further to allow varying intercepts for the persons being rated. Discuss
 #    the fitted model.Interpret the model outcome and explain what the varying Intercepts are.
+randInt_pid <- glmer(dec ~ attr + sinc + intel + fun +(1 | pid), data = speeddating, family = binomial)
+summary(randInt_pid)
 
 #(4) Now fit some models that allow the coefficients for attractiveness, compatibility, and the 
 #    other attributes to vary by person.  Fit a multilevel model, allowing the intercept and the 
 #    coefficients for the 6 ratings to vary by the rater i. (Hint: The model will not converge when you 
 #    include many predictors as random slopes; see with how many predictors you can get the model to converge;
 #    and try out some of the tricks we have seen to see whether they affect convergence for this dataset.)
-
+randInt <- glmer(dec ~ attr * sinc * intel * fun + (1 | iid) + (0 + attr + fun | iid), data = speeddating, family = binomial(logit))
+summary(randInt)
 
 #(5) compare the output for the different models that you calculated - did the model design affect your conclusions?
-
+AIC(randInt_iid, randInt_pid, randInt)
 
 ####
 #Part 2
@@ -78,7 +99,7 @@ head(speeddating)
 
 p = read.csv("poisson_sim.csv", sep=";")
 head(p)
-p <- p %>% rename(id = �..id)
+p <- p %>% rename(id = ï..id)
 p <- within(p, {
   prog <- factor(prog, levels=1:3, labels=c("General", "Academic", "Vocational"))
   id <- factor(id)
